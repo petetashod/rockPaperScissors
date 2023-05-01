b540aea48fd63e7606bb260d6a0299bb47372bf2
tashod peterson <tashodpeterson@tashods-Air.lan>
2023-04-30 22:44:09 -0400

    Final

--- script.js
+++ script.js
@@ -9,92 +9,75 @@ let rock = document.getElementById("rock");
 let paper = document.getElementById("paper");
 let scissors = document.getElementById("scissors");
 
-    
-    
-
-
-
-    //  let computerChoice = Math.floor(Math.random() * rockPaperOrScissors.length);
-    //  let computerPicks = rockPaperOrScissors[computerChoice]
-
-    
-    
-
+let computerChoice = 0;
+let computerPicks = 0;
 
 gamePlay = (userChoice) => {
-   
-    let computerChoice = Math.floor(Math.random() * rockPaperOrScissors.length);
-    let computerPicks = rockPaperOrScissors[computerChoice]
+  let computerChoice = Math.floor(Math.random() * rockPaperOrScissors.length);
+  let computerPicks = rockPaperOrScissors[computerChoice];
 
-   
-   
   if (userChoice == computerPicks) {
     document.getElementById(
       "winner"
-    ).innerHTML = `The user chose ${userChoice} and so did the computer `;
+    ).textContent= `The user chose ${userChoice} and so did the computer `;
   }
   if (userChoice == "rock" && computerPicks == "scissors") {
     document.getElementById(
       "winner"
-    ).innerHTML = `The user chose ${userChoice} and the computer chose scissors `;
+    ).textContent = `The user chose ${userChoice} and the computer chose scissors `;
   }
 
   if (userChoice == "paper" && computerPicks == "rock") {
     document.getElementById(
       "winner"
-    ).innerHTML = `The user chose ${userChoice} and the computer chose rock `;
+    ).textContent = `The user chose ${userChoice} and the computer chose rock `;
   }
 
   if (userChoice == "scissors" && computerPicks == "paper") {
     document.getElementById(
       "winner"
-    ).innerHTML = `The user chose ${userChoice} and the computer chose paper`;
+    ).textContent = `The user chose ${userChoice} and the computer chose paper`;
   }
-  
 };
 
- scoreKeeper = (userChoice) => {
-    
+scoreKeeper = (userChoice) => {
   if (
-    (computerPicks == "rock" && userChoice == "scissors") ||
-    (computerPicks == "paper" && userChoice == "rock") ||
-    (computerPicks == "scissors" && userChoice == "paper")
+    computerPicks == "rock" && userChoice == "scissors" ||
+    computerPicks == "paper" && userChoice == "rock" ||
+    computerPicks == "scissors" && userChoice == "paper"
   ) {
     computerScore++;
     document.getElementById(
       "score"
-    ).innerHTML = `computer score is ${computerScore} points`;
+    ).textContent = `computer score is ${computerScore} points`;
   }
 
   if (computerPicks == userChoice) {
-    document.getElementById("score").innerHTML = `it was a tie `;
+    document.getElementById("score").textContent = `it was a tie `;
   } else {
     userScore++;
     document.getElementById(
       "score"
-    ).innerHTML = `The user score is ${userScore} point(s)`;
+    ).textContent = `The user score is ${userScore} point(s)`;
   }
-
 };
 
 computerSelection = (userChoice) => {
-    
   if (computerPicks == "rock" && userChoice == "scissors") {
     document.getElementById(
       "score"
-    ).innerHTML = `computer chose rock and the user chose ${userChoice}`;
+    ).textContent = `computer chose rock and the user chose ${userChoice}`;
   }
   if (computerPicks == "paper" && userChoice == "rock") {
     document.getElementById(
       "score"
-    ).innerHTML = ` computer chose paper and the user chose ${userChoice} `;
+    ).textContent = ` computer chose paper and the user chose ${userChoice} `;
   }
   if (computerPicks == "scissors" && userChoice == "paper") {
     document.getElementById(
       "score"
-    ).innerHTML = ` computer chose scissors and the user chose ${userChoice} `;
+    ).textContent = ` computer chose scissors and the user chose ${userChoice} `;
   }
-
 };
 
 for (let x of choice) {
